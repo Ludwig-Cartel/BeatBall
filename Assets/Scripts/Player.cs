37fbbe4c9c4aff124ed9b269aae08fce36d286d1
CARTEL_ANGEL_LUDWIG_A <cartel1800488@mls.ceu.edu.ph>
2022-01-02 15:19:44 +0800

    BeatBall Progress 3

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,7 +8,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.visible = false;
+        //Cursor.visible = false;
         _rigidbody = GetComponent<Rigidbody>();
     }
 
